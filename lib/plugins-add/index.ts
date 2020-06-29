28943d6cb5eceba6f5b281fec6322697157a675e
Kazuki Suda <230185+superbrothers@users.noreply.github.com>
2020-06-29 23:40:36 +0900

    plugins-add: skip installing plugins already installed (#74)

--- lib/plugins-add/index.ts
+++ lib/plugins-add/index.ts
@@ -3,6 +3,31 @@ import * as exec from "@actions/exec";
 import * as fs from "fs";
 import { setupAsdf } from "../setup";
 
+const pluginList = async (): Promise<Array<string>> => {
+  let stdout = "";
+  let stderr = "";
+  const options = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        stdout += data.toString();
+      },
+      stderr: (data: Buffer) => {
+        stderr += data.toString();
+      },
+    },
+  };
+
+  try {
+    await exec.exec("asdf", ["plugin-list"], options);
+  } catch (err) {
+    if (!/No plugins installed/.test(stderr)) {
+      throw err;
+    }
+  }
+
+  return stdout.split("\n");
+};
+
 export const pluginsAdd = async () => {
   await setupAsdf();
   let toolVersions = core.getInput("tool_versions", { required: false });
@@ -23,7 +48,16 @@ export const pluginsAdd = async () => {
     .filter((x) => x.length > 0)
     .map((x) => x.split(" ")[0]);
 
+  const installedPluginNames = await pluginList();
+
   for (const pluginName of pluginNames) {
-    await exec.exec("asdf", ["plugin-add", pluginName]);
+    if (installedPluginNames.includes(pluginName)) {
+      core.info(
+        `Skip installing ${pluginName} plugin since it's already installed`
+      );
+    } else {
+      core.info(`Installing ${pluginName} plugin...`);
+      await exec.exec("asdf", ["plugin-add", pluginName]);
+    }
   }
 };
