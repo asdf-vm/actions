8f9919697fd802dcb14af07d500667d5d1026570
afirth <afirth@users.noreply.github.com>
2021-06-16 16:49:25 +0200

    chdir before exec in install

--- lib/install/index.ts
+++ lib/install/index.ts
@@ -3,6 +3,12 @@ import * as exec from "@actions/exec";
 import { pluginsAdd } from "../plugins-add";
 
 export async function toolsInstall(): Promise<void> {
+
+  const dir = core.getInput("directory", { required: false });
+  if (dir) {
+    process.chdir(dir)
+  }
+
   await pluginsAdd();
 
   const before = core.getInput("before_install", { required: false });
