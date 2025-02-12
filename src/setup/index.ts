15308c17b986ace26c7cf6f9b1489962551fb11e
Giorgio Garasto <giorgio@garasto.me>
2025-02-12 09:13:03 +0100

    feat: add support for asdf versions >= 0.16

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -4,25 +4,17 @@ import * as path from 'node:path';
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as io from '@actions/io';
+import * as httpClient from '@actions/http-client';
 
-async function setupAsdf(): Promise<void> {
-	const asdfPath = await io.which('asdf', false);
-	if (asdfPath) {
-		return;
-	}
+// NOTE: here we list only the fields we need
+type GitHubRelease = {
+	assets: ReadonlyArray<{
+		name: string;
+		url: string;
+	}>;
+};
 
-	const asdfDir = path.join(os.homedir(), '.asdf');
-	core.exportVariable('ASDF_DIR', asdfDir);
-	core.exportVariable('ASDF_DATA_DIR', asdfDir);
-	core.addPath(`${asdfDir}/bin`);
-	core.addPath(`${asdfDir}/shims`);
-
-	const skip = core.getBooleanInput('skip_install', {required: true});
-	if (skip) {
-		return;
-	}
-
-	const branch = core.getInput('asdf_branch', {required: true});
+async function installBashAsdf(asdfDir: string, branch: string): Promise<void> {
 	if (fs.existsSync(asdfDir)) {
 		core.info(`Updating asdf in ASDF_DIR "${asdfDir}" on branch "${branch}"`);
 		const options = {cwd: asdfDir};
@@ -41,6 +33,58 @@ async function setupAsdf(): Promise<void> {
 			'https://github.com/asdf-vm/asdf.git',
 			asdfDir,
 		]);
+	}
+}
+
+async function setupAsdf(): Promise<void> {
+	const asdfPath = await io.which('asdf', false);
+	if (asdfPath) {
+		return;
+	}
+
+	const asdfDir = path.join(os.homedir(), '.asdf');
+	core.exportVariable('ASDF_DIR', asdfDir);
+	core.exportVariable('ASDF_DATA_DIR', asdfDir);
+	core.addPath(`${asdfDir}/bin`);
+	core.addPath(`${asdfDir}/shims`);
+
+	const skip = core.getBooleanInput('skip_install', {required: true});
+	if (skip) {
+		return;
+	}
+
+	// If branch is provided, handle legacy (bash) asdf installation
+	const branch = core.getInput('asdf_branch', {required: false});
+	if (branch) {
+		await installBashAsdf(asdfDir, branch);
+	// Otherwise, handle installation for asdf >= 0.16
+	} else {
+		const providedVersion = core.getInput('asdf_version', {required: true});
+		const versionToFetch = providedVersion === 'latest' ? 'latest' : `tags/v${providedVersion}`;
+		const url = `https://api.github.com/repos/asdf-vm/asdf/releases/${versionToFetch}`;
+		const client = new httpClient.HttpClient();
+		const response = await client.getJson<GitHubRelease>(url);
+
+		if (response.statusCode !== 200 || !response.result) {
+			throw new Error(`Failed to fetch asdf release: ${response.statusCode}`);
+		}
+
+		const releaseToDownload = response.result.assets.find(
+			// Find the correct release based on the runner platform and architecture
+			asset => asset.name.endsWith(`-${os.platform()}-${os.arch()}.tar.gz`),
+		);
+
+		if (!releaseToDownload) {
+			throw new Error('No asdf release found for the current platform');
+		}
+
+		// Download and extract the release
+		const downloadPath = path.join(os.tmpdir(), releaseToDownload.name);
+		const extractPath = path.join(asdfDir, 'bin');
+		await exec.exec('curl', ['-sSL', '-o', downloadPath, releaseToDownload.url]);
+		await io.mkdirP(extractPath);
+		await exec.exec('tar', ['-C', extractPath, '--strip-components=1', '-xzf', downloadPath]);
+		await io.rmRF(downloadPath);
 	}
 }
 
