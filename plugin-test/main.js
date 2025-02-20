1e3bf77ae170d22c02d23121917d795868503df0
Neer Friedman <neerfri@gmail.com>
2025-02-20 13:22:54 +0200

    fix: remove "origin" argument from git checkout command

--- plugin-test/main.js
+++ plugin-test/main.js
@@ -3292,7 +3292,7 @@ async function setupAsdf() {
     const options = { cwd: asdfDir };
     await exec.exec("git", ["remote", "set-branches", "origin", branch], options);
     await exec.exec("git", ["fetch", "--depth", "1", "origin", branch], options);
-    await exec.exec("git", ["checkout", "-B", branch, "origin"], options);
+    await exec.exec("git", ["checkout", "-B", branch], options);
   } else {
     core.info(`Cloning asdf into ASDF_DIR "${asdfDir}" on branch "${branch}"`);
     await exec.exec("git", [
